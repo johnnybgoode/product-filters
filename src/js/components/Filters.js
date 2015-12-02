b2de974c65c92a4a6254e4c604d79b31aed502e4
John <johne@easternstandard.com>
2015-12-02 17:36:35 -0500

    add transitions and flickity, clean up logic in render method

--- src/js/components/Filters.js
+++ src/js/components/Filters.js
@@ -1,6 +1,8 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 var classnames = require('classnames');
 var FilterStore = require('../stores/FilterStore');
+var Flickity = require('./FlickityComponent');
 var FilterItem = require('./FilterItem');
 var FilterReset = require('./FilterReset');
 
@@ -18,36 +20,72 @@ var Filters = React.createClass({
   },
 
   render: function() {
-    var filters = this.state.filters;
-    var filterItems = Object.keys(filters).map(function(filterID) {
+    var transitionDuration = 400;
+    var flickityOptions = {
+      cellAlign: 'left',
+      cellSelector: '.filter-item',
+      initialIndex: 0,
+      percentPosition: false,
+      contain: true,
+      wrapAround: false
+    };
+
+    var filterItems = Object.keys(this.state.filters).map(function(filterID) {
+      let filter = this.state.filters[filterID];
       return (
         <FilterItem
             key={filterID}
-            active={filters[filterID].active}
-            name={filters[filterID].name}
-            tid={filters[filterID].tid}
-            image={filters[filterID].image}
+            active={filter.active}
+            name={filter.name}
+            tid={filter.tid}
+            image={filter.image}
             type={this.props.filterType}
             multi={this.props.multi}
         />
       );
     }.bind(this));
-    return (
-      <div className={classnames(
-          'filter-group', 
-          this.props.filterType + '-filters',
-          {'element-invisible': !this.state.visible}
-        )}>
-        <div className="filter-content">
-          <FilterReset
-              active={this.state.active}
-              name={this.props.filterType === 'categories' ? 'See All' : 'All'}
-              type={this.props.filterType}
-          />
-          {filterItems}
+
+    if (filterItems.length) {
+      let filters = (
+        <ReactCSSTransitionGroup
+          transitionName="filter-item"
+          transitionAppear={true}
+          transitionAppearTimeout={transitionDuration}
+          transitionEnterTimeout={transitionDuration}
+          transitionLeaveTimeout={transitionDuration - 100}>
+            <FilterReset
+                key={this.props.filterType + '-reset'}
+                active={this.state.active}
+                name={this.props.filterType === 'categories' ? 'See All' : 'All'}
+                type={this.props.filterType}
+            />
+            {filterItems}
+        </ReactCSSTransitionGroup>
+      );
+
+      if (FilterStore.state[this.props.filterType].opts.carousel) {
+        filters = (
+          <Flickity className={this.props.filterType + '-carousel'} options={flickityOptions} delay={transitionDuration}>
+              {filters}
+          </Flickity>
+        );
+      }
+
+      return (
+        <div className={classnames(
+            'filter-group',
+            this.props.filterType + '-filters',
+            {'element-invisible': !this.state.visible}
+          )}>
+          <div className="filter-content" >
+            {filters}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return ( <div /> );
+    }
   },
 
   _onChange: function() {
